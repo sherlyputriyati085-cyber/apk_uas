bd8b00155900e4da8fb73b3f288b5988fbedbe3f
username_git <sherlyputriyati085@gmail.com>
2026-05-08 18:24:50 +0700

    history screen

--- lib/main.dart
+++ lib/main.dart
@@ -1908,6 +1908,41 @@ class HistoryScreen extends ConsumerWidget {
               decoration: BoxDecoration(
                 color: Colors.orange.withValues(alpha: 0.03),
                 shape: BoxShape.circle,
-              ),
-            ),
-          ),
+              ), //Box Decoration
+            ), // Container
+          ), // Positioned
+          history.isEmpty
+              ? const Center(child: Text('Belum ada riwayat'))
+              : ListView.builder(
+                  padding: const EdgeInsets.all(20),
+                  itemCount: history.length,
+                  itemBuilder: (context, index) {
+                    final entry = history[index];
+                    return Padding(
+                      padding: const EdgeInsets.only(bottom: 24),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Column(
+                            children: [
+                              Container(
+                                padding: const EdgeInsets.all(8),
+                                decoration: BoxDecoration(
+                                  color: entry.color.withValues(alpha: 0.1),
+                                  shape: BoxShape.circle,
+                                ), //Box Decoration
+                                 child: Icon(
+                                  entry.icon,
+                                  color: entry.color,
+                                  size: 20,
+                                ), //Icon
+                              ), //Container
+                              if (index != history.length - 1)
+                                Container(
+                                  width: 2,
+                                  height: 40,
+                                  color: Colors.grey.withValues(alpha: 0.1),
+                                ), //Container
+                            ],
+                          ), //Column
+
